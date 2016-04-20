67bfae50618556400c7db53e204fb7a1e6bb79b4
Tobias Gasslander <tobias@gasslander.com>
2016-04-20 23:15:23 +0200

    skeleton csv import

---
+++ admin/import.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+
+var parser = parse({delimiter: ';'}, function(err, data) {
+
+    var participants = data.map(function(participant) {
+        return {
+            email: participant[0],
+            name: participant[3],
+            klass: participant[12],
+            gender: participant[13]
+        }
+    });
+    console.log(JSON.stringify(participants, null, 4));
+    // console.log(data);
+});
+
+process.argv.forEach(function(val, index, array) {
+
+    if(index > 1){
+
+        var filePath = array[index];
+        fs.createReadStream(filePath).pipe(parser);
+
+    }
+});
